8aa72cb04a3f3e5b1c75ceb34f7bda000ff7da24
scarvajal <scarvajal@conversantmedia.com>
2020-01-08 14:19:06 -0500

    Using Template Variables to Interact with Child Components

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -19,4 +19,9 @@ import { Component, Input } from '@angular/core';
 })
 export class EventThumbnailComponent {
   @Input() event: any
+  someProperty:any = "some value"
+
+  logFoo() {
+    console.log('foo')
+  }
 }
