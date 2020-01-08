f521c46e99c9ff17f74ecca16e1e531ca17b3ee1
scarvajal <scarvajal@conversantmedia.com>
2020-01-08 13:25:05 -0500

    Communicating with Child Components Using @Input

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'events-app',
   template: '<events-list></events-list>'
 })
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line: component-selector
   selector: 'events-list',
-  templateUrl: './events-list.component.html'
+  template: `
+    <div>
+      <h1>Upcoming Angular Events</h1>
+      <hr>
+      <event-thumbnail [event]="event"></event-thumbnail>
+    </div>
+  `
 })
 
 export class EventsListComponent {
